2454798fad9ce67cc6e06223722b8e662c796d79
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-11-12 05:12:41 +0300

    tests

--- test/test/delivery_test.go
+++ test/test/delivery_test.go
@@ -42,39 +42,31 @@ func TestSendLetter(t *testing.T) {
 	writer := MyWriter{}
 	r := http.Request{}
 	uc.SendLetter(&writer, &r)
-	fmt.Println(string(writer.Str))
 	if string(writer.Str) != string(errors.GetErrorNotPostAns()) {
-		fmt.Println("FAIL")
-		panic("Expected error is  " + string(errors.GetErrorNotPostAns()))
+		t.Errorf("Expected error is  " + string(errors.GetErrorNotPostAns()))
 	}
 	writer.Str = []byte{}
 
 	r = http.Request{Method: "POST"}
 	uc.SendLetter(&writer, &r)
-	fmt.Println(string(writer.Str))
 	if string(writer.Str) != string(errors.GetErrorUnexpectedAns()) {
-		fmt.Println("FAIL")
-		panic("Expected error is  " + string(errors.GetErrorUnexpectedAns()))
+		t.Errorf("Expected error is  " + string(errors.GetErrorUnexpectedAns()))
 	}
 
-	//writer.Str = []byte{}
-	//Letter := &LetterModel.Letter{Receiver: "dellvin.black@gmail.com", Sender: "dellvin.black@gmail.com", Theme: "dellvin.black@gmail.com",
-	//	Text: "dellvin.black@gmail.com", DateTime: int64(0)}
-	//user := UserModel.User{12323,"ds","d","ss","aa","d"}
-	//r1 := r.WithContext(context.WithValue(r.Context(),userKey{}, user))
-	////r.Context().( )
-	////r.Context.WithValue(ctx, userKey{}, user)
-	////r.Context().WithValue(ctx, userKey{}, user)
-	//fmt.Println(r.Context().Value(userKey{}))
-	////
-	//uc.SendLetter(&writer, r1)
-	//Letter := &LetterModel.Letter{Receiver: "dellvin.black@gmail.com"}
-	////mockUseCase := mock.NewMockLetterUseCase(ctrl)
-	////mockUseCase.EXPECT().SaveLetter(&Letter).Return(nil)
-	//uc := LetterDelivery.New(mockUseCase)
-	//
-	//writer := MyWriter{}
-	//r:= http.Request{}
-	//uc.SendLetter(&writer, &r)
-	//fmt.Println(writer.Str)
+	writer.Str = []byte{}
+	mockUseCase.EXPECT().GetReceivedLetters(nil).MaxTimes(0)
+	r = http.Request{Method: "POST"}
+	uc.GetRecvLetters(&writer, &r)
+	if string(writer.Str)!=string(errors.GetErrorUnexpectedAns()){
+		t.Errorf("Expected error is  " + string(errors.GetErrorUnexpectedAns()))
+	}
+
+	writer.Str = []byte{}
+	mockUseCase.EXPECT().GetSendedLetters(nil).MaxTimes(0)
+	r = http.Request{Method: "POST"}
+	uc.GetSendLetters(&writer, &r)
+	if string(writer.Str)!=string(errors.GetErrorUnexpectedAns()){
+		t.Errorf("Expected error is  " + string(errors.GetErrorUnexpectedAns()))
+	}
+
 }
