94ebcb2f6a99e41789c23ccc0aeee40408a87e2d
rufa-a <roofinda@gmail.com>
2020-12-02 20:10:12 +0300

    added simple unit tests to SmtpService

---
+++ SmtpService/internal/GetLetters/delivery_test.go
@@ -0,0 +1,73 @@
+package GetLetters
+
+import (
+	"github.com/emersion/go-smtp"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestBackend_AnonymousLogin(t *testing.T) {
+	state := &smtp.ConnectionState{}
+
+	session := &Session{}
+	back := Backend{}
+
+
+	output, err := back.AnonymousLogin(state)
+
+	assert.Nil(t, err)
+	assert.Equal(t, session, output)
+}
+
+func TestBackend_Login(t *testing.T) {
+	state := &smtp.ConnectionState{}
+	username := "username"
+	password := "password"
+
+	session := &Session{}
+	back := Backend{}
+
+
+	output, err := back.Login(state, username, password)
+
+	assert.Nil(t, err)
+	assert.Equal(t, session, output)
+}
+
+func TestSession_Data(t *testing.T) {
+	session := &Session{}
+	r := strings.NewReader("Hello, Reader!")
+	err := session.Data(r)
+	assert.Nil(t, err)
+}
+
+func TestSession_Logout(t *testing.T) {
+	session := &Session{}
+	err := session.Logout()
+	assert.Nil(t, err)
+}
+
+func TestSession_Mail(t *testing.T) {
+	session := &Session{}
+
+	opts := smtp.MailOptions{}
+	from :=  "roofinda@gmail.com"
+
+	err := session.Mail(from, opts)
+	assert.Nil(t, err)
+}
+
+func TestSession_Rcpt(t *testing.T) {
+	session := &Session{}
+	to :=  "roofinda@gmail.com"
+
+	err := session.Rcpt(to)
+	assert.Nil(t, err)
+}
+
+func TestSession_Reset(t *testing.T) {
+	session := &Session{}
+	err := session.Logout()
+	assert.Nil(t, err)
+}
---
+++ SmtpService/internal/SendLetters/delivery_test.go
@@ -0,0 +1,56 @@
+package SendLetters
+
+import (
+	"github.com/stretchr/testify/assert"
+	pb "smtpTest/proto"
+	"testing"
+)
+
+func TestSendAnswer(t *testing.T) {
+	email := "roofinda@gmail.com"
+	sendAnswer(email)
+}
+
+func TestSendAnswer2(t *testing.T) {
+	email := "roofinda@gmail.com"
+	err := SendAnswer2(email)
+	assert.NotNil(t, err)
+	//assertPanic1(t, SendAnswer2, email)
+}
+
+func TestSendLetter(t *testing.T) {
+	pbLetter := &pb.Letter{
+		Lid: 1,
+		Sender: "Sender",
+		Receiver: "Reciever",
+		Theme: "Theme",
+		Text: "Text",
+		DateTime: 1,
+	}
+
+	err := SendLetter(pbLetter)
+	assert.Nil(t, err)
+	//assertPanic2(t, SendLetter, pbLetter)
+}
+
+// https://stackoverflow.com/questions/31595791/how-to-test-panics
+//func assertPanic1(t *testing.T, f func(em string) error, email string) {
+//	defer func() {
+//		if r := recover(); r == nil {
+//			t.Errorf("The code did not panic")
+//		}
+//	}()
+//
+//	_ = f(email)
+//}
+//
+//func assertPanic2(t *testing.T, f func(letter *pb.Letter) error, letter *pb.Letter) {
+//	defer func() {
+//		if r := recover(); r == nil {
+//			t.Errorf("The code did not panic")
+//		}
+//	}()
+//
+//	err := f(letter)
+//	assert.Nil(t, err)
+//}
