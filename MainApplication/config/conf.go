31880d478fdebf910bfb64743369fb4961dabcba
marksadykov <sadykov.mark@gmail.com>
2020-12-02 19:30:06 +0300

    pic

--- MainApplication/config/conf.go
+++ MainApplication/config/conf.go
@@ -16,7 +16,7 @@ const (
 var AllowedOriginsCORS = []string{"http://localhost:80", "http://127.0.0.1:80",
 	"http://localhost", "http://127.0.0.1",
 	"http://localhost:3000", "http://127.0.0.1:3000",
-	"http://95.163.209.195:3000"}
+	"http://95.163.209.195:3000", "http://95.163.209.195:80"}
 var AllowedHeadersCORS = []string{"Version", "Authorization", "Content-Type", "csrf_token"}
 var AllowedMethodsCORS = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 
