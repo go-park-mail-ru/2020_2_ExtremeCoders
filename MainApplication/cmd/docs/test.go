d11491f1c6fdf79fbc3e317f91f7253bd5dfcfed
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-12-21 08:59:54 +0300

    tmp

---
+++ MainApplication/cmd/docs/test.go
@@ -0,0 +1,10 @@
+package docs
+
+var Text = "//post session {email:\"email@mail.ru\", password: \"password\"}- логин\n\t//delete session - логаут" +
+	"//get user - информация о пользователе\n" +
+	"\t//post user {name:\"Sam\", surname: \"Potter\", email: \"img\", password1: " +
+	"\"password1\", avatar: \"img\"}- регистрация\n\t//put user {profile_firstName:\"Sam\", profile_lastName: \"Potter\", avatar: \"img\"} - изменить профиль" +
+	"\t//get user/avatar - получить аватарку\n" +
+	"//delete letter - удалить письмо\n\t" +
+	"//post letter {to:'receiver', theme:'theme', text:'letter content'}- отправить письмо" +
+	""
