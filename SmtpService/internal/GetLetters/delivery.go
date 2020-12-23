454827a451faccb0f68d5cccd052de7e0a0951cd
Dellvin <dellvin.black@gmail.com>
2020-12-24 02:42:49 +0300

    add parse letter

--- SmtpService/internal/GetLetters/delivery.go
+++ SmtpService/internal/GetLetters/delivery.go
@@ -78,6 +78,8 @@ func (s *Session) Data(r io.Reader) error {
 	fmt.Println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 	fmt.Println(letter.Text)
 	fmt.Println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n\n\n")
+	fmt.Println(mail)
+	fmt.Println("\n\n\n||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 	resp, _:=mailManager.SaveLetter(ctx, &letter)
 	if resp.Ok==false{
 		_ = send.SendAnswerCouldNotFindUser(letter.Sender)
