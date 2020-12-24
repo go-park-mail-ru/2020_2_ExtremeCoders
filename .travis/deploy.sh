91dd5de83bfb6f3c52183f197103d45d8383d6dd
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-12-24 16:33:26 +0300

    try deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 #chmod ugo+x deploy.sh
 echo DEPLLLOY
-ssh ubuntu@95.163.209.195
+ssh ubuntu@95.163.209.195 << EOF
 pwd
-echo DEPLLLOYMachine
+echo DEPLLLOYMachine > flsfadlf.txt
 pwd
 ls
 cd go
+ls
 rm -rf 2020_2_ExtremeCoders
-git clone https://github.com/sergii1/2020_2_ExtremeCoders
+git clone https://github.com/go-park-mail-ru/2020_2_ExtremeCoders
 cd 2020_2_ExtremeCoders
+ls
 git checkout CiAndCleanArch
+git pull
 ./cmd/runAllServices.sh
+sleep 5
 cat FileService.txt
 cat MailService.txt
 cat UserService.txt
 cat MainApplication.txt
+EOF
