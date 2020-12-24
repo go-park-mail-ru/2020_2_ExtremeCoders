7c238721187162ca47fdec68f970bb947b9307e7
Dellvin <dellvin.black@gmail.com>
2020-12-24 03:21:05 +0300

    add parse letter

--- SmtpService/internal/GetLetters/delivery.go
+++ SmtpService/internal/GetLetters/delivery.go
@@ -88,9 +88,6 @@ func (s *Session) Data(r io.Reader) error {
 }
 
 func parseEmail(s string) server.Letter{
-	for ;s[0]=='\n';{
-		s=s[1:]
-	}
 	letter :=server.Letter{}
 	from := "\nFrom:"
 	subj := "\nSubject: "
