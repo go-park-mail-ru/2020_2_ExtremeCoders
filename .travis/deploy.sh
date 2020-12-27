98aa96e64caeee84c9334d237d0113eb6ab8b09b
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-12-27 19:32:01 +0300

    tmp

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,12 +12,12 @@ rm -rf 2020_2_ExtremeCoders
 git clone https://github.com/go-park-mail-ru/2020_2_ExtremeCoders
 cd 2020_2_ExtremeCoders
 ls
-git checkout CiAndCleanArch
+git checkout webSocket
 git pull
-cat ~/myconf.go > config/conf.go
-cat config/config.go
+#cat ~/myconf.go > config/conf.go
+#cat config/config.go
 ./cmd/runAllServices.sh
-sleep 5
+sleep 35
 echo res
 cat FileService.txt
 cat MailService.txt
